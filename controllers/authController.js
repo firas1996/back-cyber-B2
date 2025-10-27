316108f21c52fee67c7de6dc5062fd3a71e79dd3
firas1996 <firas.hm.1996@gmail.com>
2025-10-27 15:13:12 +0100

    aa

---
+++ controllers/authController.js
@@ -0,0 +1,24 @@
+const User = require("../models/userModel");
+
+exports.signup = async (req, res) => {
+  try {
+    console.log(req.body);
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+    });
+    res.status(201).json({
+      status: "success",
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,7 @@ const {
   updateUserById,
   deleteUserById,
 } = require("../controllers/userController");
+const { signup } = require("../controllers/authController");
 const router = express.Router(); // Create a new router object
 
 router.route("/").post(createUser).get(getAllUsers);
@@ -14,5 +15,6 @@ router
   .get(getUserById)
   .patch(updateUserById)
   .delete(deleteUserById);
+router.post("/signup", signup);
 
 module.exports = router; // Export the router to be used in other parts of the application
