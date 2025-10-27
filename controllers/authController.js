94b16e3445991df09e707e15fee5785b8a8c62cf
firas1996 <firas.hm.1996@gmail.com>
2025-10-27 16:46:57 +0100

    aaa

--- controllers/authController.js
+++ controllers/authController.js
@@ -22,3 +22,39 @@ exports.signup = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      res.status(400).json({
+        status: "fail",
+        message: "email and password are required !!!",
+      });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(400).json({
+        status: "fail",
+        message: "user does not exist !!!",
+      });
+    }
+    if (!(await user.verifyPassword(password, user.password))) {
+      res.status(400).json({
+        status: "fail",
+        message: "pass incorrect !!!",
+      });
+    }
+    res.status(201).json({
+      status: "success login",
+      //   data: {
+      //     user: newUser,
+      //   },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -43,6 +44,19 @@ const userSchema = new mongoose.Schema({
     default: Date.now(),
   },
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.confirmPassword = undefined;
+  this.password = await bcrypt.hash(this.password, 12);
+  return next();
+});
+
+userSchema.methods.verifyPassword = async function (userPass, cPass) {
+  return await bcrypt.compare(userPass, cPass);
+};
 
 const User = mongoose.model("User", userSchema);
 
